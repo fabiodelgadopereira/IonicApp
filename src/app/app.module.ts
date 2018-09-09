c30eec8ed6057b0e1047d7e8043e61c4d0c93417
Fabio Delgado <fabio.delgado2@gmail.com>
2018-09-09 17:21:15 -0300

    feed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { FeedPageModule } from '../pages/feed/feed.module';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),FeedPageModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
